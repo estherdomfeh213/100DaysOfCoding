b7270633e0cb096670111d5556f5b6164fbf4ba3
estherdomfeh213 <domfehesther2020@gmail.com>
2024-09-23 22:07:29 +0000

    Add fucntion script to calculate area of two numbers

---
+++ Functions/Practice/exercise_2.py
@@ -0,0 +1,8 @@
+# Create a function to calculate the area of a rectangle 
+
+def calculate_area(length, width):
+  """Calculate area."""
+  area = length * width
+  return area
+
+print(calculate_area(2,3))
