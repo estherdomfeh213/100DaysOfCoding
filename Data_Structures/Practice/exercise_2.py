2e4498a2650245d00a7a944d43dbfd1261d71fb0
estherdomfeh213 <domfehesther2020@gmail.com>
2024-09-26 13:04:59 +0000

    add: Dictionary methods to access key-value pairs

---
+++ Data_Structures/Practice/exercise_2.py
@@ -0,0 +1,21 @@
+favoriteBook = {
+  "title": "The Alchemist",
+  "author": "Miley",
+  "genre": "Self-development"
+  
+}
+
+
+print(favoriteBook)
+
+# use the method to retrieve the genre 
+print(favoriteBook["genre"])
+
+# * Returns a list of all the values in the dictionary
+print(favoriteBook.values())
+
+# * Returns a list containing the dictionary's keyS
+print(favoriteBook.keys())
+
+#* Return a list containing a tuple for each key value pair
+print(favoriteBook.items())
