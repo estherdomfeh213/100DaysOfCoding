54ad25c85ced873613eb6cd1a549b518eaece42f
estherdomfeh213 <domfehesther2020@gmail.com>
2024-09-26 12:45:58 +0000

    Add: Code to access second element and print.

---
+++ Data_Structures/Practice/exercise_1.py
@@ -0,0 +1,10 @@
+# Create a list to store names of your favorite fruits. Write code to access the second element and print it
+
+
+favoriteFruit = ["apple", "banana", "kiwi","mango"]
+
+# access the second element 
+print(favoriteFruit[1].capitalize())
+
+#access the last element 
+print(favoriteFruit[-1].upper())
