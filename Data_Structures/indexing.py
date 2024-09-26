09559194d5061724eafdc5048b3150d961305c98
estherdomfeh213 <domfehesther2020@gmail.com>
2024-09-26 12:18:46 +0000

    Add: Data structures in Python

---
+++ Data_Structures/indexing.py
@@ -0,0 +1,11 @@
+# Indexing is the process of accessing elements in a list by their position
+# ! How it works: In Python, indexing starts from 0 for the first element, 1 for the second, and so on. You can use square brackets [] with the index number to access the element at that position
+
+my_list = [10, 20, 30, 40, 50]
+
+print(my_list[0])
+print(my_list[2])
+
+# add a - to the index will reserve the list and access from the end of the list
+print(my_list[-1])
+print(my_list[-2])
