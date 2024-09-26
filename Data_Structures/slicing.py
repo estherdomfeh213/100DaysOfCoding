41be465758615bf9a54bd086618cf73b42d254cd
estherdomfeh213 <domfehesther2020@gmail.com>
2024-09-26 12:25:19 +0000

    Add a slicing lists in py

---
+++ Data_Structures/slicing.py
@@ -0,0 +1,10 @@
+my_list = [10, 20, 30, 40, 50]
+
+#* Using the syntax list[start:stop:step] to slice a list
+#! The start list is inclusive, the stop index is exclusive and step is the interval between element. 
+print(my_list[1:4])
+
+print(my_list[::2])
+
+# reverse the list
+print(my_list[::-1])
