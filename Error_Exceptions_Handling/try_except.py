17f7f78b9e13b30954434cd36eaf3db314721f0a
estherdomfeh213 <domfehesther2020@gmail.com>
2024-10-03 12:40:49 +0000

    Add a try_except block to handle exceptions

---
+++ Error_Exceptions_Handling/try_except.py
@@ -0,0 +1,9 @@
+try:
+  results = 10/0  # This will cause a division by zero error 
+  
+except ZeroDivisionError:
+  print("You can't divide by zero!")  # This message will be printed instead of crashing the program. 
+  
+  
+# result = 10/0 
+# print(result)
