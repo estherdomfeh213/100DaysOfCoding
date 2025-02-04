d7ef9799dea57be07cc0fbb0580d5189a3ac4817
estherdomfeh213 <domfehesther2020@gmail.com>
2025-02-04 04:12:20 +0000

    function print with two commas creates addtional space

--- cs50_python/functions_variables/hello.py
+++ cs50_python/functions_variables/hello.py
@@ -1,3 +1,11 @@
+# Ask user for their name
+# comments are notes to yourself in your code/ can also serve as pseudocode 
 name = input("What's your name? ")
+
+#* Say hello to user 
 print("hello", name)
-print(name)
+
+print("hello," + name)
+print("hello, " + name)
+
+print("hello, ", name )
