c707dcff1c910c9dc8bdaed2769b1e44562c0282
estherdomfeh213 <domfehesther2020@gmail.com>
2025-02-04 20:07:56 +0000

    function documentation

--- cs50_python/functions_variables/hello.py
+++ cs50_python/functions_variables/hello.py
@@ -14,3 +14,11 @@ print("hello", name)
 
 
 #! reading python print fuction documentation 
+"""
+print(*objects, sep=' ', end='\n', file=None, flush=False)
+Print objects to the text stream file, separated by sep and followed by end. sep, end, file, and flush, if present, must be given as keyword arguments.
+"""
+print("hello,", name, sep="???")
+print ("hello," , name, end="&&&")
+
+
