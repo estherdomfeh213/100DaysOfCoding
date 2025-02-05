542df6d53766b9bab0ed3f13aa4b626c77ef0eee
estherdomfeh213 <domfehesther2020@gmail.com>
2025-02-05 21:30:04 +0000

    practicing python basics

--- cs50_python/functions_variables/hello.py
+++ cs50_python/functions_variables/hello.py
@@ -1,9 +1,27 @@
 # Ask user for their name
 # comments are notes to yourself in your code/ can also serve as pseudocode 
-name = input("What's your name? ")
+name = input("What's your name? ").strip().title()
 
-#* Say hello to user 
-print("hello", name)
+# Split users name into first name and last name 
+first, last = name.split(" ")
+
+print(f"hello, {first}")
+print(f"hello, {last}")
+
+# #* Say hello to user 
+# print("hello", name)
+
+# # Remove whitespace from str
+# name = name.strip()
+
+# # Capitalize user's name 
+# name = name.capitalize()
+
+# name = name.title()
+
+# # Remove whitespace from str and capitalize user's name 
+# name = name.strip().title()
+
 
 # print("hello," + name)
 # print("hello, " + name)
@@ -18,7 +36,13 @@ print("hello", name)
 print(*objects, sep=' ', end='\n', file=None, flush=False)
 Print objects to the text stream file, separated by sep and followed by end. sep, end, file, and flush, if present, must be given as keyword arguments.
 """
-print("hello,", name, sep="???")
-print ("hello," , name, end="&&&")
+# print("hello,", name, sep="???")
+# print ("hello," , name, end="&&&")
 
+
+# # escape charaters \
+# print("hello \"friend\"")
+
+# # format string 
+# print(f"hello, {name}")
 
---
+++ cs50_python/practice_codes/exercise.py
@@ -0,0 +1,9 @@
+# 1. Insert the missing part of the code below to output "Hello World".
+print("Hello World")
+
+# 2. Complete the code block, print "YES" if 5 is larger than 2.
+# Hint: remember the indentation.
+# if 5 > 2:
+
+if 5 > 2:
+  print("YES")
